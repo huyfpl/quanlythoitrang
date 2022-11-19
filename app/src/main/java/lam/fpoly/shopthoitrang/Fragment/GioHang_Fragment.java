eee4eeb0b21fa19716e2ee4d842fc81dc356395e
Duy Lam <111013821+Duylamjunpio@users.noreply.github.com>
2022-11-19 18:44:23 +0700

    bottom nav

---
+++ app/src/main/java/lam/fpoly/shopthoitrang/Fragment/GioHang_Fragment.java
@@ -0,0 +1,22 @@
+package lam.fpoly.shopthoitrang.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import lam.fpoly.shopthoitrang.R;
+
+
+public class GioHang_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gio_hang, container, false);
+    }
+}

46658756e369cb1b392afd15bf12fc9afb4beb7d
Huy Mr <huy339093@gmail.com>
2022-12-07 17:28:11 +0700

    update mhc

--- app/src/main/java/lam/fpoly/shopthoitrang/manhinhcho/manhinhcho.java
+++ app/src/main/java/lam/fpoly/shopthoitrang/manhinhcho/manhinhcho.java
@@ -11,10 +11,14 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import lam.fpoly.shopthoitrang.AccFragment.DangNhapActivity;
 import lam.fpoly.shopthoitrang.Dao.TbLogoDao;
@@ -24,14 +28,19 @@ import lam.fpoly.shopthoitrang.Model.TbLogo;
 import lam.fpoly.shopthoitrang.R;
 
 public class manhinhcho extends AppCompatActivity {
-    RelativeLayout manhinhchao;
+    LinearLayout manhinhchao;
     LottieAnimationView loadingmanhinhchao;
     TbLogoDao tbLogoDao;
+    TextView clock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhcho);
-
+        clock=findViewById(R.id.clock);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateformat = new SimpleDateFormat("hh:mm a");
+        String datetime = dateformat.format(c.getTime());
+        clock.setText(datetime);
         tbLogoDao=new TbLogoDao();
         manhinhchao=findViewById(R.id.manhinhchao);
         loadingmanhinhchao=findViewById(R.id.loadingmanhinhchao);
